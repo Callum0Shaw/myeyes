f5abaafa9ed2783ba135cd46187f1e3c257a79a7
Callum Shaw <callum0shaw@gmail.com>
2023-02-15 12:45:56 +0200

    feat: add hero blok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import vercel from '@astrojs/vercel/serverless';
+import mkcert from 'vite-plugin-mkcert';
 import { loadEnv } from 'vite';
-
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    storyblok({
-      accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
-      apiOptions: {},
-      bridge: true,
-      components: {
-        about: 'storyblok/About',
-        page: 'storyblok/Page'
-      },
-      useCustomApi: false,
-    })
-  ],
+  integrations: [storyblok({
+    accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
+    apiOptions: {},
+    bridge: true,
+    components: {
+      about: 'storyblok/About',
+      hero: 'storyblok/Hero',
+      page: 'storyblok/Page',
+    },
+    useCustomApi: false
+  })],
+  vite: {
+    server: {
+      https: true
+    },
+    plugins: [mkcert()]
+  }
 });
