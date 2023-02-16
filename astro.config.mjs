f02bb1cac47c7b0104783b757566027cdb4d9161
Callum Shaw <callum0shaw@gmail.com>
2023-02-16 10:17:06 +0200

    feat: basic css/html structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import vercel from '@astrojs/vercel/serverless';
+import image from '@astrojs/image';
 import mkcert from 'vite-plugin-mkcert';
 import { loadEnv } from 'vite';
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
@@ -9,18 +10,25 @@ const env = loadEnv("", process.cwd(), 'STORYBLOK');
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [storyblok({
-    accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
-    apiOptions: {},
-    bridge: true,
-    components: {
-      about: 'storyblok/About',
-      form: 'storyblok/Form',
-      hero: 'storyblok/Hero',
-      page: 'storyblok/Page',
-    },
-    useCustomApi: false
-  })],
+  integrations: [
+    image(),
+    storyblok({
+      accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
+      apiOptions: {},
+      bridge: true,
+      components: {
+        about: 'storyblok/About',
+        form: 'storyblok/Form',
+        hero: 'storyblok/Hero',
+        page: 'storyblok/Page',
+        row_of_2: 'storyblok/RowOf2',
+        row_of_3: 'storyblok/RowOf3',
+        screening: 'storyblok/Screening',
+        things: 'storyblok/Things'
+      },
+      useCustomApi: false
+    })
+  ],
   vite: {
     server: {
       https: true
