054feeba8d20179e21ec098c0f03424bde5d8636
Callum Shaw <callum0shaw@gmail.com>
2023-03-30 17:17:34 +0300

    feat: test netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,36 +4,34 @@ import vercel from '@astrojs/vercel/serverless';
 import image from '@astrojs/image';
 import mkcert from 'vite-plugin-mkcert';
 import { loadEnv } from 'vite';
+import netlify from "@astrojs/netlify/functions";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
-
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  integrations: [
-    image(),
-    storyblok({
-      accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
-      apiOptions: {},
-      bridge: true,
-      components: {
-        about: 'storyblok/About',
-        aboutPage: 'storyblok/AboutPage',
-        allShows: 'storyblok/AllShows',
-        allThings: 'storyblok/AllThings',
-        form: 'storyblok/Form',
-        highlightedThings: 'storyblok/HighlightedThings',
-        nextShow: 'storyblok/NextShow',
-        page: 'storyblok/Page',
-        previousShows: 'storyblok/PreviousShows',
-        row_of_2: 'storyblok/RowOf2',
-        showPage: 'storyblok/ShowPage',
-        thingPage: 'storyblok/ThingPage',
-        things: 'storyblok/Things'
-      },
-      useCustomApi: false
-    })
-  ],
+  adapter: netlify(),
+  integrations: [image(), storyblok({
+    accessToken: 'r9ie4IIkISoPVfCgX7Kx7Att',
+    apiOptions: {},
+    bridge: true,
+    components: {
+      about: 'storyblok/About',
+      aboutPage: 'storyblok/AboutPage',
+      allShows: 'storyblok/AllShows',
+      allThings: 'storyblok/AllThings',
+      form: 'storyblok/Form',
+      highlightedThings: 'storyblok/HighlightedThings',
+      nextShow: 'storyblok/NextShow',
+      page: 'storyblok/Page',
+      previousShows: 'storyblok/PreviousShows',
+      row_of_2: 'storyblok/RowOf2',
+      showPage: 'storyblok/ShowPage',
+      thingPage: 'storyblok/ThingPage',
+      things: 'storyblok/Things'
+    },
+    useCustomApi: false
+  })],
   vite: {
     server: {
       https: true
